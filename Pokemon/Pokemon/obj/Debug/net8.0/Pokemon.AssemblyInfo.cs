d114894ee8989e071af6fc7980c750045243d9da
DEBCAF <andrew.wong14597295@gmail.com>
2025-07-07 23:04:27 +0800

    changed json

--- Pokemon/Pokemon/obj/Debug/net8.0/Pokemon.AssemblyInfo.cs
+++ Pokemon/Pokemon/obj/Debug/net8.0/Pokemon.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pokemon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2150d5be906d2a9dcd16cee978fa6f0cdab65141")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pokemon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pokemon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
