12aa03ff03058dfa010ee5eec852d49e25585476
DEBCAF <andrew.wong14597295@gmail.com>
2025-09-19 13:09:17 +0100

    added new linked list

---
+++ LinkedLidst/Program.cs
@@ -0,0 +1,41 @@
+﻿class LinkedLidst
+{
+    private int maxSize;
+    private int size;
+    private string[] items;
+    private int start;
+    private int free;
+    public LinkedLidst(int maxSize)
+    {
+        this.maxSize = maxSize;
+        items = new string[maxSize];
+        start = -1;
+        free = 0;
+    }
+
+    public boolean isFull()
+    {
+        if (maxSize == size)
+        {
+            return true;
+        }
+        return false;
+    }
+    class node
+    {
+        private string data;
+        private int pointer;
+
+        public node(string data, int pointer)
+        {
+            this.pointer = pointer;
+            this.data = data;
+        }
+    }
+
+    public boolean add(string value)
+    {
+        if 
+    }
+
+}
